bc17318aa8a98ee806d6980be3bcc2f010c3c098
Shathurya P <shathuparam27@gmail.com>
2024-04-29 19:26:13 +0530

    error resolved

--- backend/app.js
+++ backend/app.js
@@ -8,8 +8,6 @@ const chat = require("./routes/chat");
 const cart = require("./routes/cart");
 const wishlist = require("./routes/wishList");
 const register_S = require("./routes/register_S");
-
-const register_S=require("./routes/register_S");
 const dashboard_S =require("./routes/dashboard_S");
 const cookieParser = require("cookie-parser");
 
