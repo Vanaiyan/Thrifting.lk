936cc0755f28267e1ba19619ba6286b73a3a612e
Aruran Kirupanantha <arukirupa@gmail.com>
2024-07-01 10:33:16 +0530

    Product Card changes

--- frontend/src/Components/Cards/Productcardlg.js
+++ frontend/src/Components/Cards/Productcardlg.js
@@ -1,33 +1,37 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-import IconButton from "@mui/material/IconButton";
-import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
-import FavoriteIcon from "@mui/icons-material/Favorite";
+import { useNavigate } from "react-router-dom";
 import Typography from "@mui/material/Typography";
 import WishlistIconButton from "../WishList/WishListIcon";
+import { Box } from "@mui/material";
+import AddToCartButton from "../Cart/AddtoCartBtn";
 import { pushInteractedProduct } from "../../Actions/homeProductActions"; // Adjust path as necessary
+import { useDispatch } from "react-redux";
 
-export default function ProductCardlg({ title, price, imageSrc, id }) {
-  const [isWishlist, setIsWishlist] = useState(false);
-
+export default function ProductCardlg({ id, title, price, imageSrc }) {
+  const [recommendations, setRecommendations] = useState([]);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
   const handleCardClick = async () => {
-    // Call the pushInteractedProduct action
     try {
-      await pushInteractedProduct(id);
+      await dispatch(pushInteractedProduct(id));
+      navigate(`/productDetail/${id}`);
     } catch (error) {
       console.error("Error pushing interacted product:", error);
       // Handle error as needed
     }
   };
 
+  const handleButtonClick = (event) => {
+    // Prevent the card click event from being triggered
+    event.stopPropagation();
+  };
+
   return (
     <Card
-      component={Link}
-      to={`/productDetail/${id}`}
       sx={{
         width: { lg: "200px", md: "180px", sm: "160px", xs: "150px" },
         height: { lg: "300px", md: "290px", sm: "290px", xs: "290px" },
@@ -39,34 +43,42 @@ export default function ProductCardlg({ title, price, imageSrc, id }) {
           boxShadow: "0px 10px 33px rgba(0, 0, 0, 0.3)",
         },
       }}
-      onClick={handleCardClick} // Call pushInteractedProduct on card click
+      onClick={handleCardClick} // Call handleCardClick on card click
     >
-      <CardMedia sx={{ height: 200 }} image={imageSrc} title={title} />
-      <CardContent
-        sx={{
-          border: "1px #DEE2E7",
-          height: 100,
-          marginBottom: "10px",
-          transition: "background-color 0.4s ease-in", // Increased transition time to 2 seconds
-          "&:hover": {
-            bgcolor: "#D9D9D9",
-          },
-        }}
-      >
+      <CardMedia sx={{ height: 170 }} image={imageSrc} title={title} />
+      <CardContent>
         <Typography
-          variant="subtitle2"
+          fontSize="14px"
           component="div"
-          sx={{ color: "dimgrey" }}
+          sx={{ fontWeight: 600, position: "absolute", top: "170px" }}
         >
           {title}
-          {id}
         </Typography>
-        <Typography variant="subtitle2">Price: {price}</Typography>
+        <Typography
+          fontSize="14px"
+          color="text.secondary"
+          sx={{ position: "absolute", top: "210px" }}
+        >
+          Price: {price}
+        </Typography>
+        {recommendations.length > 0 && (
+          <Typography fontSize="12px" sx={{ marginTop: "30px" }}>
+            <strong>Recommended:</strong> {recommendations.join(", ")}
+          </Typography>
+        )}
       </CardContent>
-
-      <CardActions sx={{ position: "absolute", top: 0, right: 0 }}>
-        <WishlistIconButton />
+      <CardActions
+        sx={{ display: "flex", alignItems: "flex-end", marginTop: "15px" }}
+        onClick={handleButtonClick}
+      >
+        <AddToCartButton productId={id} />
       </CardActions>
+      <Box
+        sx={{ position: "absolute", top: 0, right: 0 }}
+        onClick={handleButtonClick}
+      >
+        <WishlistIconButton productId={id} />
+      </Box>
     </Card>
   );
 }
