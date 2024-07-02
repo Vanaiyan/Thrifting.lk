545d503f178599eb3e5155e556a9d40f242b24ba
Tharmasuki <tharmasukij1215@gmail.com>
2024-07-02 11:12:05 +0530

    email and correct the admin product card

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -1,18 +1,45 @@
+// const nodemailer = require("nodemailer");
+// require("dotenv").config();
+
+// const sendEmail = async (options) => {
+//   const transport = nodemailer.createTransport({
+//     host: "sandbox.smtp.mailtrap.io",
+//     port: 2525,
+//     auth: {
+//       user: process.env.SMTP_USER,
+//       pass: process.env.SMTP_PASS,
+//     },
+//   });
+
+//   const message = {
+//     from: '"vanai" <vanaiyan@gmail.com>',
+//     to: options.to,
+//     subject: options.subject,
+//     text: options.text,
+//   };
+
+//   await transport.sendMail(message);
+// };
+
+// module.exports = sendEmail;
+
+
+
+
 const nodemailer = require("nodemailer");
 require("dotenv").config();
 
 const sendEmail = async (options) => {
   const transport = nodemailer.createTransport({
-    host: "sandbox.smtp.mailtrap.io",
-    port: 2525,
+    service: "gmail",
     auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS,
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD,
     },
   });
 
   const message = {
-    from: '"vanai" <vanaiyan@gmail.com>',
+    from: `"${process.env.SMTP_FROM_NAME}" <${process.env.SMTP_FROM_EMAIL}>`,
     to: options.to,
     subject: options.subject,
     text: options.text,
@@ -22,3 +49,4 @@ const sendEmail = async (options) => {
 };
 
 module.exports = sendEmail;
+
