c1bd8e266ebaa9d0053be88273dd8bf17842ee60
Vanaiyan <129414316+Vanaiyan@users.noreply.github.com>
2024-07-03 17:30:33 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://thrifting-lk-fe.onrender.com" || "http://localhost:3000",
     credentials: true,
   })
 );
